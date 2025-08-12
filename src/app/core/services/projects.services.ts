c1f75a9dca864bd52b4b138283b02bb0a575edfa
shahd293mohamed <123604275+shahd293mohamed@users.noreply.github.com>
2025-08-13 01:47:00 +0300

    Update projects.services.ts

--- src/app/core/services/projects.services.ts
+++ src/app/core/services/projects.services.ts
@@ -11,7 +11,6 @@ export class ProjectsService {
 
   constructor(private http: HttpClient) { }
 
-  // Get all projects
   getProjects(): Observable<Iprojects[]> {
     return this.http.get<Iprojects[]>(this.baseUrl);
   }
@@ -19,17 +18,14 @@ export class ProjectsService {
   getProjectById(id: string) {
   return this.http.get<Iprojects>(`${this.baseUrl}/${id}`);
 }
-  // Add a new project (with image upload)
   addProject(formData: FormData): Observable<Iprojects> {
     return this.http.post<Iprojects>(this.baseUrl, formData);
   }
 
-  // Update a project
   updateProject(id: string, formData: FormData): Observable<Iprojects> {
     return this.http.put<Iprojects>(`${this.baseUrl}/${id}`, formData);
   }
 
-  // Delete a project
   deleteProject(id: string): Observable<any> {
     return this.http.delete(`${this.baseUrl}/${id}`);
   }
