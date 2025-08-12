2a6e2e1787bb0314fd2be02685713c9529dbb860
shahd293mohamed <123604275+shahd293mohamed@users.noreply.github.com>
2025-08-13 01:46:00 +0300

    Update about.services.ts

--- src/app/core/services/about.services.ts
+++ src/app/core/services/about.services.ts
@@ -1,6 +1,3 @@
-// 
-
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -18,27 +15,12 @@ export class AboutService {
     return this._http.get<Iabout[]>(`${this.baseUrl}/aboutus`);
   }
 
-  // createAbout(about: Iabout): Observable<Iabout> {
-  //   return this._http.post<Iabout>(`${this.baseUrl}/aboutus`, about);
-  // }
-
-  // updateAbout(id: string, about: Iabout): Observable<Iabout> {
-  //   return this._http.put<Iabout>(`${this.baseUrl}/aboutus/${id}`, about);
-  // }
   createAbout(formData: FormData): Observable<Iabout[]> {
   return this._http.post<Iabout[]>(`${this.baseUrl}/aboutus`, formData);
 }
 
-// updateAbout(formData: FormData): Observable<Iabout> {
-//   return this._http.put<Iabout>(`${this.baseUrl}/aboutus`, formData);
-// }
 updateAbout(_id: string, formData: FormData): Observable<Iabout> {
   return this._http.patch<Iabout>(`${this.baseUrl}/aboutus/${_id}`, formData);
 }
 
-
-
-  // getProjects():Observable<Iprojects[]> {
-  //   return this._http.get<Iprojects[]>(`${this.baseUrl}/projects`);
-  // }
 }
